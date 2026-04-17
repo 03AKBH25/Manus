d1bdff47db7c8e6cbe1c7d8fff355a10baa3146c
03AKBH25 <25aniket9@gmail.com>
2026-04-18 00:06:07 +0530

    initial commit

---
+++ backend/src/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+
+// Security middleware
+app.use(helmet());
+
+// CORS used for cross origin connection
+app.use(cors());
+
+// Logging errors and requests for better debugging
+app.use(morgan("dev"));
+
+// Body parser
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    message: "Server running is running don't you dare meddle with it!"
+  });
+});
+
+export default app;
